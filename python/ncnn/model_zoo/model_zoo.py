6a787583cd8f0f9dca3915ed7275596c46620b40
caishanli <caishanli25@gmail.com>
2020-03-03 09:07:35 +0800

    add example rfcn

--- python/ncnn/model_zoo/model_zoo.py
+++ python/ncnn/model_zoo/model_zoo.py
@@ -8,7 +8,7 @@ from .squeezenet import SqueezeNet
 from .fasterrcnn import Faster_RCNN
 from .peleenetssd import PeleeNet_SSD
 from .retinaface import RetinaFace
-#from .rfcn import RFCN
+from .rfcn import RFCN
 from .shufflenetv2 import ShuffleNetV2
 from .simplepose import SimplePose
 
@@ -25,7 +25,7 @@ _models = {
             'faster_rcnn' : Faster_RCNN,
             "peleenet_ssd" : PeleeNet_SSD,
             "retinaface" : RetinaFace,
-            #"rfcn" : RFCN,
+            "rfcn" : RFCN,
             "shufflenetv2" : ShuffleNetV2,
             "simplepose" : SimplePose,
         }
